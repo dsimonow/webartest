b61aab1de3a5eba9f7f343f86a3dfba3b16cef29
deschimi <dimitrisimonow90@googlemail.com>
2023-05-24 14:41:15 +0000

    Create Examples.stories.tsx

---
+++ src/stories/3D/Examples.stories.tsx
@@ -0,0 +1,40 @@
+// Examples.stories.ts|tsx
+
+
+import {Setup} from '../Setup'
+import { Blob, Logo, Duck, Dog } from '../../components/canvas/Examples';
+
+
+
+export default {
+  title: '3D/Examples',
+  component: Blob,
+}
+
+export function AssetBlob(){
+  return <Blob />
+}
+
+AssetBlob.storyName = 'Blob'
+AssetBlob.decorators = [(storyFn) => <Setup cameraPosition={new Vector3(10, 10, 10)}>{storyFn()}</Setup>]
+
+export function AssetLogo(){
+  return <Logo />
+}
+
+AssetLogo.storyName = 'Logo'
+AssetLogo.decorators = [(storyFn) => <Setup cameraPosition={new Vector3(10, 10, 10)}>{storyFn()}</Setup>]
+
+export function AssetDuck(){
+  return <Duck />
+}
+
+AssetDuck.storyName = 'Duck'
+AssetDuck.decorators = [(storyFn) => <Setup cameraPosition={new Vector3(10, 10, 10)}>{storyFn()}</Setup>]
+
+export function AssetDog(){
+  return <Dog />
+}
+
+AssetDog.storyName = 'Dog'
+AssetDog.decorators = [(storyFn) => <Setup cameraPosition={new Vector3(10, 10, 10)}>{storyFn()}</Setup>]
