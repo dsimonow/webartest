cf78adeb9ff992c16bfd39e89d0a574d7428e845
Dimitri <dimitrisimonow90@googlemail.com>
2023-05-30 10:35:43 +0200

    push

--- src/components/canvas/Examples.test.ts
+++ src/components/canvas/Examples.test.ts
@@ -2,13 +2,13 @@ import ReactThreeTestRenderer  from "@react-three/test-renderer";
 import { composeStories } from '@storybook/react';
 import * as stories from './Examples.stories'
 
-const { Blob, Logo, Duck, Dog} = composeStories(stories);
+const { BlobExample, LogoExample, DuckExample, DogExample} = composeStories(stories);
 
 test('renders Blob default', async () => {
-    //const renderer = await ReactThreeTestRenderer.create(<Blob />)
+    const renderer = await ReactThreeTestRenderer.create(<BlobExample />)
     
 
-    //expect(mesh.length).toBe(2)
+    expect(mesh.length).toBe(2)
 }
 
 )
