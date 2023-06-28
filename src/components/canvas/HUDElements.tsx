aba288847c45cf5c5410c229ac5e9669bbfca42d
deschimi <dimitrisimonow90@googlemail.com>
2023-06-28 22:47:34 +0000

    Update HUDElements.tsx and yarn.lock

--- src/components/canvas/HUDElements.tsx
+++ src/components/canvas/HUDElements.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useScenarioStore, useTransformControlsStore, useFireSimulationStore } from "./store";
-import { Sphere, Hud, OrthographicCamera, ScreenSpace, Text } from "@react-three/drei";
+import { Sphere, Hud, OrthographicCamera, ScreenSpace, Text, Box, Billboard, Html } from "@react-three/drei";
 import { Interactive} from "@react-three/xr";
 import { useThree } from '@react-three/fiber'
 import { useState } from "react";
@@ -24,61 +24,52 @@ export function HUDElements() {
                 <ambientLight intensity={1} />
                 <pointLight position={[200, 200, 100]} intensity={0.5} />
                 <OrthographicCamera makeDefault position={[0, 0, 50]}>
-                    <group visible={!sceneStarted}>
+                    <group  visible={!sceneStarted}>
                     <Interactive onSelect={() => {
                         // XR Deactivate and Hide
                         if(!sceneStarted){
                             setFireState(0, "ignite")
                             setStartScene(true)
-                        }   
-                    }
-                        }>
-                        <Text color={"black"} scale={10} position={[75, -130, -450]}>Feuer</Text>
-                        <Sphere
-                            position={[75, -130, -500]}
-                            scale={15}
+                        }}}>
+                          <Sphere
+                          position={[75, -130, -500]}
+                            scale={[15,15,1]}
                             onClick={() => {
-                                // XR Deactivate and Hide
-                                if (!sceneStarted) {
-                                    setFireState(0, "ignite")
-                                    setStartScene(true)
-                                }
-                            }
-                        }
-                        >
-                            <meshPhysicalMaterial color={'#1E90FF'} />
-                        </Sphere>
+                                  // XR Deactivate and Hide
+                                  if (!sceneStarted) {
+                                      setFireState(0, "ignite")
+                                      setStartScene(true)
+                                  }}}>
+                              <meshPhysicalMaterial color={'#1E90FF'} />
+                          </Sphere>
                     </Interactive>
                     </group>
                     <Interactive onSelect={() => increaseScenarioSize()}>
-                        <Text color={"black"} scale={20} position={[110, -130, -400]} >+</Text>
-                        <Sphere 
+                        <Sphere
                         position={[110, -130, -500]} 
-                        scale={15} 
+                        scale={[15,15,1]} 
                         onClick={() => increaseScenarioSize()} 
                         >
                         <meshPhysicalMaterial color={scenarioSize < 0.010 ? ('#DFFF00') : '#32CD32'} />
-                        </Sphere>
+                        </Sphere >
                     </Interactive>
                     <Interactive onSelect={() => decreaseScenarioSize()}>
-                        <Text color={"black"} scale={30} position={[145, -130, -400]} >-</Text>
-                        <Sphere
+                        <Sphere 
                             position={[145, -130, -500]}
-                            scale={15}
+                            scale={[15,15,1]}
                         onClick={() => decreaseScenarioSize()}
                         >
                         <meshPhysicalMaterial color={scenarioSize > 0.0001 ? ('#808000') : '#1E90FF'} />
-                        </Sphere>
+                        </Sphere >
                     </Interactive>
                     <Interactive onSelect={() => toggleTransformControlEnabled()}>
-                    <Text color={"black"} scale={10} position={[180, -130, -400]}>Rotate</Text>
-                        <Sphere 
+                        <Sphere  
                         position={[180, -130, -500]} 
-                        scale={15} 
+                        scale={[15,15,1]}
                         onClick={() => toggleTransformControlEnabled()} 
                         >
                             <meshPhysicalMaterial color={transformControlEnabled === true ? ('#7CFC00') : '#B22222'} />
-                        </Sphere>
+                        </Sphere >
                     </Interactive>
                 </OrthographicCamera>
             </Hud>
